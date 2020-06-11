aa4460e4c5e777e7417a4e463e88038ae4696ae0
bmishra <bmishra@OHTW-TEC-BMISHA>
2020-06-11 11:23:25 -0400

    initial checkin

---
+++ CalendarEmitter/Emitter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace CalendarEmitter
+{
+  public class Emitter
+  {
+    public void PrintMonth(int year, int month)
+    {
+      bool isCurrentMonthnYear = year == DateTime.Now.Year && month == DateTime.Now.Month;
+
+      PrintCalenderheader(year, month);
+
+      const string seperator = "\t";
+      var days = Enum.GetValues(typeof(DayOfWeek));
+
+      int daysinMonth = getDaysinCurrentMonth(year, month);
+      int dayoftheWeek = (int)(new DateTime(year, month, 1)).DayOfWeek;
+      int numberofWeeksToShow = daysinMonth == 28 && dayoftheWeek == (int)DayOfWeek.Sunday ? 4 : 5;
+      int startDay = (-1) * (dayoftheWeek - 1);
+
+      foreach (var day in days)
+      {
+        Console.Write(day.ToString().Substring(0,2));
+        Console.Write(seperator);
+      }
+
+      Console.WriteLine();
+
+      for (int i = 0; i < numberofWeeksToShow; i++)
+      {
+        for (int j = 0; j < 7; j++)
+        {
+          if (isCurrentMonthnYear && startDay == DateTime.Now.Day)
+          {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("{0}", startDay > 0 ? startDay.ToString() : string.Empty);
+            Console.ResetColor();
+          }
+          else
+          {
+            Console.Write("{0}", startDay > 0 ? startDay.ToString() : string.Empty);
+          }
+
+          Console.Write(seperator);
+          if (startDay < daysinMonth) startDay++;
+          else break;
+        }
+
+        Console.Write('\n');
+      }
+    }
+
+    private void PrintCalenderheader(int year, int month)
+    {
+      Console.WriteLine("\t\t{0} {1}", MonthName(month).ToUpperInvariant(), year);
+    }
+
+    private string MonthName(int month)
+    {
+      switch (month)
+      {
+        case 1: return "January";
+        case 2: return "February";
+        case 3: return "March";
+        case 4: return "April";
+        case 5: return "May";
+        case 6: return "June";
+        case 7: return "July";
+        case 8: return "August";
+        case 9: return "Sepetember";
+        case 10: return "October";
+        case 11: return "November";
+        case 12: return "December";
+        default: return "none";
+      }
+    }
+
+    private int getDaysinCurrentMonth(int year, int month)
+    {
+      return DateTime.DaysInMonth(year, month);
+    }
+  }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,64 @@
+﻿using CalendarEmitter;
+using System;
+
+namespace Client
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int year = 0;
+      int month = 0;
+
+      Console.WriteLine("Kindly enter the command and hit enter. To exit hit Q/q!");
+      Console.WriteLine("Ex: cal, cal <year>, cal <month> <year>.");
+      string commandName = Console.ReadLine().Trim().ToLowerInvariant();
+      while (commandName != "q")
+      {
+        commandName = commandName.Trim();
+
+        if (!commandName.StartsWith("cal"))
+          Console.WriteLine("Incorrect command.calen");
+
+        string[] commandParameters = commandName.Split(' ');
+
+        if (commandParameters.Length == 2 && commandParameters[1].Length == 4)
+        {
+          int.TryParse(commandParameters[1], out year);
+        }
+
+        if (commandParameters.Length == 3 && commandParameters[1].Length <= 2 && commandParameters[2].Length == 4)
+        {
+          int.TryParse(commandParameters[1], out month);
+          int.TryParse(commandParameters[2], out year);
+        }
+
+        Emitter objEmitter = new Emitter();
+
+        if (commandParameters.Length == 1 && commandParameters[0].ToLowerInvariant() == "cal")
+        {
+          objEmitter.PrintMonth(DateTime.Now.Year, DateTime.Now.Month);
+        }
+        else if (commandParameters.Length == 2 && commandParameters[0].ToLowerInvariant() == "cal" && year != 0)
+        {
+          for (int m = 1; m <= 12; m++)
+          {
+            objEmitter.PrintMonth(year, m);
+            Console.WriteLine();
+          }
+        }
+        else if (commandParameters.Length == 3 && commandParameters[0].ToLowerInvariant() == "cal" && year != 0 && month != 0)
+        {
+          objEmitter.PrintMonth(year, month);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Kindly enter the command and hit enter. To exit hit Q/q!");
+        commandName = Console.ReadLine().Trim().ToLowerInvariant();
+
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
